d7f822b1aad600e82c2659fc7d26f31387610bed
Pooja Hake <83300990+poojahake@users.noreply.github.com>
2023-08-28 13:42:16 +0530

    ASCII VALUE

---
+++ AsciiValue .java
@@ -0,0 +1,10 @@
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+	
+		        char ch = 'a';
+		        int Ascii = ch;
+		        System.out.println("ASCII value of " + ch + " is : " + Ascii);
+		    }
+		}
