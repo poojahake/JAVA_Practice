fe9d378bfb12d47607055237488b696b484881ad
Pooja Hake <83300990+poojahake@users.noreply.github.com>
2023-08-28 13:13:16 +0530

    java program for basic calculation

---
+++ Calculations.java
@@ -0,0 +1,18 @@
+public class Calculations {
+
+	public static void main(String[] args) {
+		int a,b,add,sub,mul,div;
+		a=10;
+		b=5;
+		add=a+b;
+		sub=a-b;
+		mul=a*b;
+		div=a/b;
+		System.out.println("Additon of "+ a+" and "+b+" is "+add);
+		System.out.println("Subtraction of "+ a+" and "+b+" is "+sub);
+		System.out.println("Multiplication of "+ a+" and "+b+" is "+mul);
+		System.out.println("divide of "+ a+" and "+b+" is "+div);
+
+	}
+
+}
