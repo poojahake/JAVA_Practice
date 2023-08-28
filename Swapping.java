218c854b7c24d6af539c49b8b56a13164c1a25f0
Pooja Hake <83300990+poojahake@users.noreply.github.com>
2023-08-28 13:24:33 +0530

    Swap numbers using third variable

---
+++ Swapping.java
@@ -0,0 +1,17 @@
+public class SwappingNumber {
+
+	public static void main(String[] args) {
+		int a,b,thirdVariable = 0;
+		a=10;
+		b=5;
+		System.out.println("Before swapping Number is"+" a=" +a +" and "+" b="+b);
+		thirdVariable=a;      //c=5
+		 a=b;         //a=10
+		 b=thirdVariable;     //b=5
+		 
+		 System.out.println("After swapping Number is"+" a=" +a +" and "+" b="+b);
+	   
+		
+	}
+
+}
