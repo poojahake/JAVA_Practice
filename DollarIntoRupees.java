b4a3e7ffad802775828bc620b98f5f76766efdb0
Pooja Hake <83300990+poojahake@users.noreply.github.com>
2023-08-31 11:07:36 +0530

    Convert Dollars to Rupees

---
+++ DollarIntoRupees.java
@@ -0,0 +1,9 @@
+public class DollarIntoRupees {
+
+	public static void main(String[] args) {
+		float Dollar=15;
+		float rupees=Dollar*82.65f;
+		System.out.println(+Dollar+" Dollar in Rupess is equal  to RS "+rupees);
+	}
+
+}
